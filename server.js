c99a9a65f17aa3447a63a6513f9b832ce525f1a5
Ed Lee <hjlee@Eds-MbP.local>
2015-08-15 14:46:58 -0700

    fixed an error in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var port = process.ENV.PORT || 8000
+var port = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname, './client')));
 
