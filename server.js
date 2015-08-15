b981610de1402cc96fb02ede0baa0e0a2f5da6b5
Ed Lee <hjlee@Eds-MbP.local>
2015-08-15 14:44:26 -0700

    made app heroku ready

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var port = process.ENV.PORT || 8000
+
 app.use(express.static(path.join(__dirname, './client')));
 
 require('./config/routes.js')(app);
 
-app.listen(8000, function() {
-	console.log('app listening on port 8000');
+app.listen(port, function() {
+	console.log('app listening on port' + port);
 });
